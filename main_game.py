0d7c37196272a5109e3837123e5ace63bd44ebd6
RishatTalukder <120198784+RishatTalukder@users.noreply.github.com>
2023-07-16 18:11:17 +0600

    updated

--- main_game.py
+++ main_game.py
@@ -1,56 +1,30 @@
-#pygame screen
-from sys import exit
 import pygame
+from sys import exit
+
 pygame.init()
+
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption("Game")
+pygame.display.set_caption('LAFALAFI')
 clock = pygame.time.Clock()
 
-test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+# test_surface = pygame.Surface((500,80))
+# test_surface.fill('Red')
+# test_surface2 = pygame.Surface((300,200))
+# test_surface2.fill('Green')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+player_surface = pygame.image.load('graphics/Player/player_stand.png')
 
-sky_surface = pygame.image.load('graphics/Sky.png').convert()
-ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('Hello World', True, (255,25,255))
-
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surface.get_rect(midbottom = (700,300))
-
-snail_x_position = 600
-
-
-player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-
-player_rect = player_surface.get_rect(midbottom = (80,300))
-
-#game loop
-running = True
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-        if event.type == pygame.MOUSEMOTION:
-            print(event.pos)
-
+    # screen.blit(test_surface,(0,0))
+    # screen.blit(test_surface2,(50,70))
     screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
-    snail_rect.right -= 4
-
-    if snail_rect.right <=0:
-        snail_rect.left = 800
-    
-    screen.blit(snail_surface,snail_rect)
-    player_rect.left += 2
-    screen.blit(player_surface,player_rect)
-
-    # if player_rect.colliderect(snail_rect):
-    #     print("shit")
-    mouse_pos = pygame.mouse.get_pos()
-    if player_rect.collidepoint(mouse_pos):
-        print(pygame.mouse.get_pressed())
-
+    screen.blit(ground_surface,(0,250))
+    screen.blit(player_surface,(20,160))
     pygame.display.update()
     clock.tick(60)
-
