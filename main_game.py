937e9d2ceda4efc034ee7bdb1abb156772043a84
RishatTalukder <120198784+RishatTalukder@users.noreply.github.com>
2023-07-21 20:23:21 +0600

    updated

--- main_game.py
+++ main_game.py
@@ -7,13 +7,20 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('LAFALAFI')
 clock = pygame.time.Clock()
 
+#text_font = pygame.font.SysFont('Arial',22)
+text_font = pygame.font.Font('font/Pixeltype.ttf',50)
+
 # test_surface = pygame.Surface((500,80))
 # test_surface.fill('Red')
 # test_surface2 = pygame.Surface((300,200))
 # test_surface2.fill('Green')
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-player_surface = pygame.image.load('graphics/Player/player_stand.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+player_surface = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+enemy_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+enemy_x_pos = 700
+
+text_surface = text_font.render("LAFALAFI",False,'Black')
 
 while True:
     for event in pygame.event.get():
@@ -26,5 +33,11 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,250))
     screen.blit(player_surface,(20,160))
+    screen.blit(text_surface,(350,50))
+    screen.blit(enemy_surface,(enemy_x_pos,210))
+    enemy_x_pos -= 4
+    if enemy_x_pos < -50:
+        enemy_x_pos = 900
+
     pygame.display.update()
     clock.tick(60)
