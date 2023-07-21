6eda6097835c719dc66c53fef55cd606d510fb67
RishatTalukder <120198784+RishatTalukder@users.noreply.github.com>
2023-07-21 20:52:27 +0600

    update

--- main_game.py
+++ main_game.py
@@ -17,7 +17,9 @@ text_font = pygame.font.Font('font/Pixeltype.ttf',50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 player_surface = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80,250))
 enemy_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+enemy_rect = enemy_surface.get_rect(midbottom = (700,250))
 enemy_x_pos = 700
 
 text_surface = text_font.render("LAFALAFI",False,'Black')
@@ -32,12 +34,14 @@ while True:
     # screen.blit(test_surface2,(50,70))
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,250))
-    screen.blit(player_surface,(20,160))
+    screen.blit(player_surface,player_rect)
     screen.blit(text_surface,(350,50))
-    screen.blit(enemy_surface,(enemy_x_pos,210))
-    enemy_x_pos -= 4
-    if enemy_x_pos < -50:
-        enemy_x_pos = 900
+    screen.blit(enemy_surface,enemy_rect)
+    enemy_rect.x -= 4
+    if enemy_rect.right <= 0: enemy_rect.left = 800
+    # enemy_x_pos -= 4
+    # if enemy_x_pos < -50:
+    #     enemy_x_pos = 900
 
     pygame.display.update()
     clock.tick(60)
